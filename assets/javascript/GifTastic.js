5ff6c80d03d72ac2f447db1231534f56f37837da
Joe Gonzalez <joseph.gonzalez111@gmail.com>
2019-03-05 18:58:53 -0800

    commit!

---
+++ assets/javascript/GifTastic.js
@@ -0,0 +1,6 @@
+
+
+
+// MY API KEY : scHFOTlr5QbpLqi7BYJlSZqjewWlg0a8
+
+// REQUEST URL : https://api.giphy.com/v1/gifs/search?api_key=scHFOTlr5QbpLqi7BYJlSZqjewWlg0a8&q=&limit=25&offset=0&rating=PG&lang=en
